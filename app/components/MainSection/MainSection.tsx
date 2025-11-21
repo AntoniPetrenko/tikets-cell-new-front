60dc352a2a62427aaf0329d46d819a21370aa31d
Anton Petrenko <a.petrenko@sirinsoftware.com>
2025-11-21 13:01:44 +0200

    feat: added animated background

--- app/components/MainSection/MainSection.tsx
+++ app/components/MainSection/MainSection.tsx
@@ -1,31 +1,16 @@
 "use client";
 
-import Image from "next/image";
-import BMW from "../../../public/bmw-m5.jpg";
-
 export const MainSection = () => {
   return (
-    <section className="relative w-full min-h-screen flex items-center justify-center">
-      <Image
-        src={BMW}
-        alt="Background"
-        fill
-        priority
-        className="object-cover object-center z-10"
-      />
+    <section className=" w-full min-h-screen flex items-center justify-center">
 
-      <div className="absolute inset-0 bg-black/40 z-10" />
-
-      <div className="relative z-10 text-white text-center p-8">
+      <div className="relative z-10 text-white text-center p-8" style={{ maxWidth: "800px" }}>
         <h1 className="text-4xl md:text-5xl font-bold mb-4">
-          Підбір авто та запчастин для будь-яких потреб
+          Підбір авто та запчастин
         </h1>
-        <p className="font-medium text-lg">
+        <p className="font-medium text-lg" style={{ marginTop: "2rem" }} >
           Ми допоможемо вам обрати автомобіль, який ідеально відповідає вашому
-          стилю життя та бюджету. Наша команда підбирає перевірені машини по
-          всій Україні та забезпечує якісні комплектуючі для будь-якої моделі.
-          Ми дбаємо про кожну деталь, щоб ви отримали надійний транспорт і
-          комфорт на кожному кілометрі шляху.
+          стилю життя та бюджету.
         </p>
       </div>
     </section>
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,23 @@ import "./globals.css";
 import { MainSection } from "./components/MainSection/MainSection";
 import { AboutUsSection } from "./components/AboutUsSection/AboutUsSection";
 import { CatalogSection } from "./components/CatalogSection/CatalogSection";
+import FloatingLines from "@/app/components/FloatingLines";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-dark font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col items-center justify-between dark:bg-black sm:items-start text-6xl font-bold">
+        <FloatingLines
+          enabledWaves={['top', 'middle', 'bottom']}
+          // Array - specify line count per wave; Number - same count for all waves
+          lineCount={[5, 3, 10]}
+          // Array - specify line distance per wave; Number - same distance for all waves
+          lineDistance={[8, 6, 4]}
+          bendRadius={5.0}
+          bendStrength={-0.5}
+          interactive={true}
+          parallax={true}
+        />
         <MainSection />
         <AboutUsSection />
         <CatalogSection />
